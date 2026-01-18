8b334d2ae09779b0a1088bf674e21c477569cf41
Aromalpriyan <aromalpriyan213@gmail.com>
2026-01-18 20:29:39 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,13 @@ function App(){
   return (
     <div className="bg-stone-800 text-white min-h-screen relative overflow-hidden">
         <img src={img} alt="" className="absolute inset-0 w-full h-full object-cover"/>
-        <div className="relative mt-25">
-          <h1 className="text-amber-400 text-center text-4xl lg:text-6xl py-10 font-bold font-head">Cat Fact</h1>
-          <div className="text-center">
+        <div className="relative min-h-screen flex items-center">
+          <div className="ml-10 lg:ml-24 max-w-xl">
+            <h1 className="text-amber-400 text-4xl lg:text-6xl py-10 font-bold font-head">Cat Fact</h1>
+            <div>
             <h1 className="font-fact font-bold text-2xl lg:text-3xl">{catFact}</h1>
             <button onClick={fetchData} className=" bg-sky-700 hover:bg-sky-800 text-2xl lg:text-3xl lg:px-8 lg:py-2 mt-24 px-4 py-2 cursor-pointer rounded-md font-semibold font-button transition-all duration-300 ">Generate CatFact</button>
+            </div>
           </div>
         </div>
 
